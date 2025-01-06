ed0b1c03ae2524fa00c2ab4c139159a231673e39
Mitch <mresch01@gmail.com>
2025-01-05 18:30:26 -0600

    updates

--- src/main/java/liquibase/ext/athena/lockservice/LockServiceAthena.java
+++ src/main/java/liquibase/ext/athena/lockservice/LockServiceAthena.java
@@ -33,66 +33,73 @@ public class LockServiceAthena extends StandardLockService {
 
     @Override
     public void init() throws DatabaseException {
-        boolean createdTable = false;
+        
         Executor executor = Scope.getCurrentScope().getSingleton(ExecutorService.class).getExecutor("jdbc", database);
+        executor.execute(new CreateDatabaseChangeLogLockTableStatement());
+        database.commit();
+        super.init();
+        
+    //     boolean createdTable = false;
+    //     Executor executor = Scope.getCurrentScope().getSingleton(ExecutorService.class).getExecutor("jdbc", database);
 
-        int maxIterations = 10;
+    //     int maxIterations = 10;
+        
 
-        if (executor instanceof LoggingExecutor) {
-            if (super.isDatabaseChangeLogLockTableCreated()) {
-                maxIterations = 0;
-            } else {
-                maxIterations = 1;
-            }
-        }
+    //     if (executor instanceof LoggingExecutor) {
+    //         if (super.isDatabaseChangeLogLockTableCreated()) {
+    //             maxIterations = 0;
+    //         } else {
+    //             maxIterations = 1;
+    //         }
+    //     }
 
-        for (int i = 0; i < maxIterations; i++) {
-            try {
-                if (!super.isDatabaseChangeLogLockTableCreated(true)) {
-                    executor.comment("Create Database Lock Table");
-                    Scope.getCurrentScope().getLog(getClass()).fine(
-                        "Creating database lock table with name: " +
-                            database.escapeTableName(
-                                database.getLiquibaseCatalogName(),
-                                database.getLiquibaseSchemaName(),
-                                database.getDatabaseChangeLogLockTableName()
-                            )
-                    );
-                    executor.execute(new CreateDatabaseChangeLogLockTableStatement());
-                    database.commit();
-                    Scope.getCurrentScope().getLog(getClass()).fine(
-                        "Created database lock table with name: " +
-                            database.escapeTableName(
-                                database.getLiquibaseCatalogName(),
-                                database.getLiquibaseSchemaName(),
-                                database.getDatabaseChangeLogLockTableName()
-                            )
-                    );
-                    this.hasDatabaseChangeLogLockTable = true;
-                    createdTable = true;
-                }
+    //     for (int i = 0; i < maxIterations; i++) {
+    //         try {
+    //             if (!super.isDatabaseChangeLogLockTableCreated(true)) {
+    //                 executor.comment("Create Database Lock Table");
+    //                 Scope.getCurrentScope().getLog(getClass()).fine(
+    //                     "Creating database lock table with name: " +
+    //                         database.escapeTableName(
+    //                             database.getLiquibaseCatalogName(),
+    //                             database.getLiquibaseSchemaName(),
+    //                             database.getDatabaseChangeLogLockTableName()
+    //                         )
+    //                 );
+    //                 executor.execute(new CreateDatabaseChangeLogLockTableStatement());
+    //                 database.commit();
+    //                 Scope.getCurrentScope().getLog(getClass()).fine(
+    //                     "Created database lock table with name: " +
+    //                         database.escapeTableName(
+    //                             database.getLiquibaseCatalogName(),
+    //                             database.getLiquibaseSchemaName(),
+    //                             database.getDatabaseChangeLogLockTableName()
+    //                         )
+    //                 );
+    //                 this.hasDatabaseChangeLogLockTable = true;
+    //                 createdTable = true;
+    //             }
 
-                if (!isDatabaseChangeLogLockTableInitialized(createdTable, true)) {
-                    executor.comment("Initialize Database Lock Table");
-                    executor.execute(new InitializeDatabaseChangeLogLockTableStatement());
-                    database.commit();
-                }
+    //             if (!isDatabaseChangeLogLockTableInitialized(createdTable, true)) {
+    //                 executor.comment("Initialize Database Lock Table");
+    //                 executor.execute(new InitializeDatabaseChangeLogLockTableStatement());
+    //                 database.commit();
+    //             }
 
-                break;
-            } catch (Exception e) {
-                if (i == maxIterations - 1) {
-                    throw e;
-                } else {
-                    Scope.getCurrentScope().getLog(getClass()).fine("Failed to create Athena databasechangeloglock table.  Attempting again in 1 second");
-                    database.rollback();
-                    try {
-                        Thread.sleep(random.nextInt(1000));
-                    } catch (InterruptedException ex) {
-                        Scope.getCurrentScope().getLog(getClass()).warning("Sleep interrupted while waiting to retry creating databasechangeloglock table", ex);
-                        Thread.currentThread().interrupt();
-                    }
-                }
-            }
-        }
+    //             break;
+    //         } catch (Exception e) {
+    //             if (i == maxIterations - 1) {
+    //                 throw e;
+    //             } else {
+    //                 Scope.getCurrentScope().getLog(getClass()).fine("Failed to create Athena databasechangeloglock table.  Attempting again in 1 second");
+    //                 database.rollback();
+    //                 try {
+    //                     Thread.sleep(random.nextInt(1000));
+    //                 } catch (InterruptedException ex) {
+    //                     Scope.getCurrentScope().getLog(getClass()).warning("Sleep interrupted while waiting to retry creating databasechangeloglock table", ex);
+    //                     Thread.currentThread().interrupt();
+    //                 }
+    //             }
+    //         }
+    //     }
     }
 }
